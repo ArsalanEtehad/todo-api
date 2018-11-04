8e23e2883c361883014729f2ad32de6bcd9b97bb
Arsalan <z5163778@ad.unsw.edu.au>
2018-11-04 14:51:09 +1100

    find by a specific field

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -8,7 +8,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client)=>{
     const db = client.db('TodoApp');
 
     //the find().toArray() return the promise. so we use then...
-    db.collection('Todos').find().toArray().then((docs)=>{
+    db.collection('Todos').find({completed: true}).toArray().then((docs)=>{
       console.log('Todos: ')
       console.log(JSON.stringify(docs,undefined,2))
 
