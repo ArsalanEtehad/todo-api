0f86c7ec3a626b3bbd84310169d603ab4d068b1f
Arsalan <z5163778@ad.unsw.edu.au>
2018-11-04 13:23:57 +1100

    Connect to Mango in Node and insert 1 document

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,9 +1,35 @@
 const MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=>{
+//no need to create TodoApp database before trying to connect to it. It will be created if not exist.
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client)=>{
     if(err){
       return console.log('Unable to connect to MongoDb server') //either return or put the rest of the code in an else block.
     }
+
     console.log('Connected to MongoDb server.')
-    db.close()
+
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').insertOne({
+    //   text: 'life has just began',
+    //   completed: false
+    // }, (err, result)=>{ //(err, result)  : a callback
+    //   if(err){
+    //     return console.log('Unable to insert todo', err);
+    //   }
+    //   console.log(JSON.stringify(result.ops, undefined, 2))
+    // })
+
+    db.collection('Users').insertOne({
+      name: 'Arsalan',
+      age: 27,
+      location: 'Sydney, Australia'
+    }, (err, result)=>{
+      if(err){
+        return console.log('Unable to insert Users', err)
+      }
+      console.log(JSON.stringify(result.ops, undefined, 2))
+    })
+
+    client.close()
 })
