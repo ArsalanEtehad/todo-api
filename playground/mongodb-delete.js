420a71d16924c6cb6148fb5875ae03ee78d1ad59
Arsalan <z5163778@ad.unsw.edu.au>
2018-11-04 20:43:32 +1100

    deleteMany, deleteOne and findOneAndDelete

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -7,11 +7,31 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client)=>{
     console.log('Connected to MongoDb server.')
     const db = client.db('TodoApp');
 
-    db.collection('Todos').findOneAndDelete({text:'Eat lunch'}).then((docs)=>{
-      console.log('deleted: ',docs)
+    // deleteMany
+    // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((results)=>{
+    //   console.log(results)
+    // }).catch((err)=>{
+    //   console.log(err)
+    // })
 
+    //deleteOne
+    // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result)=>{
+    //   console.log(result)
+    // }).catch((err)=>{
+    //   console.log(err)
+    // })
+
+    // deleteMany
+    // db.collection('Users').deleteMany({name: 'Arsalan'})
+
+    //findOneAndDelete
+    db.collection('Users').findOneAndDelete({
+      _id: new ObjectID('5bde5ae18196acb56c6dd6b6')
+    }).then((result)=>{
+      console.log(result)
     }).catch((err)=>{
       console.log(err)
     })
+
     // client.close()
 })
