fac544681ab283d878614662f4a15ef07b8d627d
Arsalan <z5163778@ad.unsw.edu.au>
2018-11-04 15:24:27 +1100

    findOneAndDelete

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -1,24 +1,17 @@
-// const MongoClient = require('mongodb').MongoClient;
 const {MongoClient, ObjectID} = require('mongodb')  //just the alternative way of casting these 2 - deconstructioning
 
-//no need to create TodoApp database before trying to connect to it. It will be created if not exist.
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client)=>{
     if(err){
       return console.log('Unable to connect to MongoDb server') //either return or put the rest of the code in an else block.
     }
     console.log('Connected to MongoDb server.')
-
     const db = client.db('TodoApp');
 
-    db.collection('Todos').insertOne({
-      text: 'Eat lunch',
-      completed: false
-    }, (err, result)=>{
-      if(err){
-        return console.log('Unable to insert Users', err)
-      }
-      console.log(result.ops)
-    })
+    db.collection('Todos').findOneAndDelete({text:'Eat lunch'}).then((docs)=>{
+      console.log('deleted: ',docs)
 
-    client.close()
+    }).catch((err)=>{
+      console.log(err)
+    })
+    // client.close()
 })
