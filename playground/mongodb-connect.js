6ee73bf953ceea3593b4a878bebbc1642a0ed11c
Arsalan <z5163778@ad.unsw.edu.au>
2018-11-04 14:11:10 +1100

    playing around with _id object in mongo

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,13 @@
-const MongoClient = require('mongodb').MongoClient;
+// //Object Destructioning:
+// var user = {name: 'Arsalan', age: 27}
+// var {name, text, age} = user //age is equal to user's age now.
+// console.log(name, text, age) //log:  Arsalan undefined 27
+
+//Similar to above example of Object Destruction:
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb')  //just the alternative way of casting these 2
+var obj = new ObjectID()
+console.log(obj)
 
 //no need to create TodoApp database before trying to connect to it. It will be created if not exist.
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client)=>{
@@ -19,16 +28,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client)=>{
     //   console.log(JSON.stringify(result.ops, undefined, 2))
     // })
 
-    db.collection('Users').insertOne({
-      name: 'Arsalan',
-      age: 27,
-      location: 'Sydney, Australia'
-    }, (err, result)=>{
-      if(err){
-        return console.log('Unable to insert Users', err)
-      }
-      console.log(result.ops[0]._id.getTimestamp())
-    })
+    // db.collection('Users').insertOne({
+    //   name: 'Arsalan',
+    //   age: 27,
+    //   location: 'Sydney, Australia'
+    // }, (err, result)=>{
+    //   if(err){
+    //     return console.log('Unable to insert Users', err)
+    //   }
+    //   console.log(result.ops)
+    // })
 
     client.close()
 })
