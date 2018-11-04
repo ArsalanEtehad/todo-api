6cc13f0a6d01d7480eb82237682b355b04570ab6
Arsalan <z5163778@ad.unsw.edu.au>
2018-11-04 15:00:07 +1100

    using count() in mongodb driver api

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -7,16 +7,25 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client)=>{
     console.log('Connected to MongoDb server.')
     const db = client.db('TodoApp');
 
-    //the find().toArray() return the promise. so we use then...
-    db.collection('Todos').find({
-      _id: new ObjectID('5bde669017335629aa1066dd')
-    }).toArray().then((docs)=>{
-      console.log('Todos: ')
-      console.log(JSON.stringify(docs,undefined,2))
+    // //the find().toArray() return the promise. so we use then...
+    // db.collection('Todos').find({
+    //   _id: new ObjectID('5bde669017335629aa1066dd')
+    // }).toArray().then((docs)=>{
+    //   console.log('Todos: ')
+    //   console.log(JSON.stringify(docs,undefined,2))
+    //
+    // }).catch((err)=>{
+    //   console.log('Unable to fetch todos',err)
+    // })
 
+    //the find().toArray() return the promise. so we use then...
+    db.collection('Todos').find().count().then((count)=>{
+      console.log(`Todos count:${count} `)
+      
     }).catch((err)=>{
       console.log('Unable to fetch todos',err)
     })
+
 
     // client.close()
 })
