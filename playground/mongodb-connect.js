090f45ea0d6871dc22c3c9a05c61fd8ac390de08
Arsalan <z5163778@ad.unsw.edu.au>
2018-11-04 13:24:36 +1100

    Connect to Mango in Node and insert 1 document

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -5,7 +5,6 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client)=>{
     if(err){
       return console.log('Unable to connect to MongoDb server') //either return or put the rest of the code in an else block.
     }
-
     console.log('Connected to MongoDb server.')
 
     const db = client.db('TodoApp');
