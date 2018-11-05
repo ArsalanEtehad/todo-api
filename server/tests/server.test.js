14e2de2fcb438db23044565b09cec4012534ffa7
Arsalan <z5163778@ad.unsw.edu.au>
2018-11-05 15:30:19 +1100

    tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,11 +4,29 @@ const request = require('supertest')
 const {app} = require('./../server')
 const {Todo} = require('./../models/todo')
 
+
+const todo_list_for_test = [
+  {
+    text: 'todo text no 1'
+  },{
+    text: 'todo text no 2'
+  },{
+    text: 'todo text no 3'
+  }
+];
+
 //to make sure we start with 0 todos in db
 //it will be revoked BEFORE EACH it() test
+// beforeEach((done)=>{
+//   Todo.remove({}).then(()=>done())
+// })
+
 beforeEach((done)=>{
-  Todo.remove({}).then(()=>done())
+  Todo.remove({}).then(()=>{
+    return Todo.insertMany(todo_list_for_test)
+  }).then(()=>done())
 })
+
 
 describe('Post /todos', ()=>{
   it('shoude create a new todo',(done)=>{ //done because it's async
@@ -30,8 +48,8 @@ describe('Post /todos', ()=>{
       }
 
       Todo.find().then((todos)=>{
-        expect(todos.length).toBe(1) //because at line 9 we remove all the documents in db. so it's always 1
-        expect(todos[0].text).toBe(text)
+        expect(todos.length).toBe(4) //because at line 9 we remove all the documents in db. so it's always 1
+        expect(todos[3].text).toBe(text)
         done()
       }).catch((err)=>{
         done(err);
@@ -51,7 +69,7 @@ describe('Post /todos', ()=>{
     .end(function(err, res) {
       if (err) return done(err);
       Todo.find().then((todos)=>{
-        expect(todos.length).toBe(0) //because at line 9 we remove all the documents in db. so it's always 1
+        expect(todos.length).toBe(3) //because at line 9 we remove all the documents in db. so it's always 1
         done()
       }).catch((err)=>{
         done(err);
@@ -67,7 +85,7 @@ describe('Post /todos', ()=>{
     .end(function(err, res) {
       if (err) return done(err);
       Todo.find().then((todos)=>{
-        expect(todos.length).toBe(0) //because at line 9 we remove all the documents in db. so it's always 1
+        expect(todos.length).toBe(3) //because at line 9 we remove all the documents in db. so it's always 1
         done()
       }).catch((err)=>{
         done(err);
@@ -75,5 +93,18 @@ describe('Post /todos', ()=>{
     });
   })
 
+});
 
+
+describe('Post /todos', ()=>{
+  it('should get all todos',(done)=>{
+    request(app)
+    .get('/todos')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .expect((res)=>{
+      expect(res.body.todos.length).toBe(3)
+    })
+    .end(done)
+  })
 })
