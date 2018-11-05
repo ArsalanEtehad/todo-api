8d149e50bda1768daa61a35135076ee69f298b78
Arsalan <z5163778@ad.unsw.edu.au>
2018-11-05 11:54:18 +1100

    more on mongoose validation

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ var Todo = mongoose.model('Todo', {
 
 //creating a document of Todo model [mongoose model]
 var newTodo = new Todo({
-  text: 'Cook dinner'
+  text: 'Feed the Cat'
 })
 
 //saving the created Todo document to the mongodb [database]
@@ -32,4 +32,27 @@ newTodo.save().then((doc)=>{
   console.log('successfully saved newTodo: ',doc)
 },(e)=>{
   console.log('Unable to save todo')
+})
+
+var User = mongoose.model('User',{
+  name: {
+    type: String
+  },
+  email:{
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  }
+})
+
+var user1 = new User({
+  name: 'John',
+  email: ' johndoe4@gmail.com'
+})
+
+user1.save().then((res)=>{
+  console.log('successfully added ',res)
+}).catch((err)=>{
+  console.log('Failed to add user: ', err)
 })
