10d866a261a3c612b21d38c946b019627723234d
Arsalan <z5163778@ad.unsw.edu.au>
2018-11-05 14:25:04 +1100

    2 tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,6 +5,7 @@ const {app} = require('./../server')
 const {Todo} = require('./../models/todo')
 
 //to make sure we start with 0 todos in db
+//it will be revoked BEFORE EACH it() test
 beforeEach((done)=>{
   Todo.remove({}).then(()=>done())
 })
@@ -27,6 +28,7 @@ describe('Post /todos', ()=>{
       if (err){
         return done(err);
       }
+
       Todo.find().then((todos)=>{
         expect(todos.length).toBe(1) //because at line 9 we remove all the documents in db. so it's always 1
         expect(todos[0].text).toBe(text)
@@ -34,20 +36,26 @@ describe('Post /todos', ()=>{
       }).catch((err)=>{
         done(err);
       })
+
     })
 
   })
 
+
   it('should not create todo with invalid data', (done)=>{
     var text = ""
-
     request(app)
     .post('/todos')
     .send({text}) //or ({text: text}) I just used ES6 notation to make it simple
     .expect(400)
     .end(function(err, res) {
       if (err) return done(err);
-      done();
+      Todo.find().then((todos)=>{
+        expect(todos.length).toBe(0) //because at line 9 we remove all the documents in db. so it's always 1
+        done()
+      }).catch((err)=>{
+        done(err);
+      })
     });
   })
 })
