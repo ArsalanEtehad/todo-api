8c0ae32b738a045f4493d7c0c783055ee44a077e
Arsalan <z5163778@ad.unsw.edu.au>
2018-11-05 20:41:24 +1100

    removing , { useNewUrlParser: true }

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 //grasping Promise of global library to mongoose...
 mongoose.Promise = global.Promise
 //Asynch connection
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 
 module.exports={
