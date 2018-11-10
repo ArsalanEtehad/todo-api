848e8d78e60d6a0164f0ad52c76e72c713ccd67c
Arsalan <z5163778@ad.unsw.edu.au>
2018-11-10 16:51:13 +1100

    authenticate.js middleware

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const _ = require('lodash')
 const {ObjectID} = require('mongodb')
 
+var {authenticate} = require('./middleware/authenticate')
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo')
 var {User}= require('./models/user')
@@ -111,27 +112,6 @@ app.post('/users', (req, res)=>{
   })
 });
 
-
-//++++++++++++++++++++++++++
-
-//midleware
-var authenticate = (req, res, next)=>{
-  var token = req.header('x-auth'); //in line 108 we send the header('x-auth') by res and now we taking it to use it
-
-  User.findByToken(token).then((user)=>{
-    if(!user){
-      return Promise.reject();
-    }
-    // res.send(user);
-    req.user = user;
-    req.token = token;
-    next();
-
-  }).catch((err)=>{
-    res.status(401).send() //401 status: Unauthorized: authentication is required/
-  })
-
-}
 //--------------------------GET /users/me----------------------------
 app.get('/users/me', authenticate,(req,res)=>{
   res.send(req.user);
