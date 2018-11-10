39c805fc01e5ea14ebb14c393c9d27112d406eba
Arsalan <z5163778@ad.unsw.edu.au>
2018-11-11 10:19:24 +1100

    Added _creator and authentication to TODOS patch and delete

--- server/server.js
+++ server/server.js
@@ -45,14 +45,17 @@ app.get('/todos',authenticate, (req,res)=>{
 });
 
 //------------------------GET /todos/:id------------------------------
-app.get('/todos/:id',(req,res)=>{
+app.get('/todos/:id',authenticate, (req,res)=>{
   var id = req.params.id
   //validating the id in url
   if(!ObjectID.isValid(id)){
     return res.status(404).send()
   }
   //searching in todo db by the id
-  Todo.findById(id).then((todo)=>{
+  Todo.findOne({
+    _id: id,
+    _creator: req.user._id
+  }).then((todo)=>{
     if(!todo){
       return res.status(404).send()
     }
@@ -63,12 +66,15 @@ app.get('/todos/:id',(req,res)=>{
 });
 
 //------------------------DELETE /todos/:id------------------------------
-app.delete('/todos/:id', (req, res)=>{
+app.delete('/todos/:id',authenticate ,(req, res)=>{
   var id = req.params.id;
   if(!ObjectID.isValid(id)){
     return res.status(404).send();
   }
-  Todo.findByIdAndDelete(id).then((todo)=>{
+  Todo.findOneAndDelete({
+    _id: id,
+    _creator: req.user._id
+  }).then((todo)=>{
     if(!todo){
       return res.status(404).send();
     }
@@ -79,14 +85,14 @@ app.delete('/todos/:id', (req, res)=>{
 });
 
 //------------------------PATCH /todos/:id------------------------------
-app.patch('/todos/:id', (req, res)=>{
+app.patch('/todos/:id', authenticate, (req, res)=>{
   var id = req.params.id;
   var body = _.pick(req.body, ['text']);
 
   if(!ObjectID.isValid(id)){
     return res.status(404).send();
   }
-  Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+  Todo.findOneAndUpdate({_id:id, _creator:req.user._id},{$set:body},{new:true}).then((todo)=>{
     if(!todo){
       return res.status(404).send();
     }
