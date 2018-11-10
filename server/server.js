53078dadfed39bc8aaa62a0d704d8e62cbf2950d
Arsalan <z5163778@ad.unsw.edu.au>
2018-11-10 23:53:49 +1100

    POST /users/login with TOkens

--- server/server.js
+++ server/server.js
@@ -100,14 +100,13 @@ app.post('/users/login',(req, res)=>{
   var body = _.pick(req.body, ['email','password']);
 
   User.findByCredentials(body.email, body.password).then((user)=>{
-    res.send(user);
+    return user.generateAuthToken().then((token)=>{
+    res.header('x-auth', token).send(user);
   }).catch((e)=>{
     res.status(400).send();
   })
-
-
-
-});
+})
+})
 
 //------------------------POST /users------------------------------
 app.post('/users', (req, res)=>{
